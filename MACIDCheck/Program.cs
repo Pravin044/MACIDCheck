2e028015b74917fa18a257fe960b11ce2fa5b702
pravin kharade <pravin.pk044@gmail.com>
2019-04-04 13:56:06 +0530

    Add project files.

---
+++ MACIDCheck/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace MACIDCheck
+{
+    class Program
+    {
+
+        public string  getMACWithNetworkInterface()
+        {
+            NetworkInterface[] netWorkInterface = NetworkInterface.GetAllNetworkInterfaces();
+            var ethernet = netWorkInterface.Where(network => network.NetworkInterfaceType == NetworkInterfaceType.Ethernet && network.Name.Equals("Ethernet")).FirstOrDefault();
+            String sMacAddress = ethernet == null ? string.Empty : ethernet.GetPhysicalAddress().ToString();
+            return sMacAddress;
+               
+        }
+
+       
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            Console.WriteLine("MACID with Network Interface : {0}",pg.getMACWithNetworkInterface());
+            Console.ReadKey();
+
+        }
+    }
+}
